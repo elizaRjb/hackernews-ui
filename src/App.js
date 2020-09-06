7495428ba80ca7366de1e8c025e226b81caae6d5
elizaRjb <elizarajbhandari@lftechnology.com>
2020-09-06 11:11:49 +0545

    Add header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import Component from 'components/Component';
+import Header from 'components/header/Header';
+
+import 'assets/sass/style.css';
 
 function App() {
   return (
     <div className="App">
-      <Component />
+      <Header />
     </div>
   );
 }
